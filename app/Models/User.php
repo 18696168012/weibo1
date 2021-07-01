df0e9e48abdcadf56e8db4bd790f4ae847c1abcd
vagrant <vagrant@homestead>
2021-07-01 01:43:30 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function gravatar(){
+        $path = public_path();
+        return '/image/tx.jpg';
+    }
 }
