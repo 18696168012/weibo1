bd3e5620a10740ee567104f0e7fe47fee2341fa3
vagrant <vagrant@homestead>
2021-07-02 02:32:37 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,4 +28,28 @@ class UsersController extends Controller
         Auth::login($user);
         return redirect()->route('users.show',[$user]);
     }
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|mix:6'
+        ]);
+        $data=[];
+        if($request->password){
+            $data['password']=$request->password;
+        }
+        $data['name']=$request->name;
+        $re=$user->update($data);
+        if($re){
+            //成功
+            session()->flash('success','更新成功');
+            return redirect()->route('users.show',[$user]);
+        }else{
+            //失败
+            session()->flash('danger','更新失败');
+            return redirect()->back()->withInput();
+        }
+    }
 }
