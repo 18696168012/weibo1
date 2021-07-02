b7b98daa372d389d200ad2e2d4a1190e081e1fd6
vagrant <vagrant@homestead>
2021-07-02 10:41:47 +0000

    数据填充

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(100)->create();
+        $user=User::find(1);
+        $user->name='张宏扬';
+        $user->email='791264638@qq.com';
+        $user->password=bcrypt(123456);
+        $user->save();
+    }
+}
