533cf3552124046fa4bb3a40b26ec5dd810322c5
vagrant <vagrant@homestead>
2021-07-02 01:22:45 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsCOntroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsCOntroller extends Controller
+{
+    public function create(){
+        return view('sessions.create');
+    }
+    public function store(Request $request){
+        $param=$this->validate($request,[
+            'email'=>'required|email|min:6',
+            'password'=>'required',
+        ]);
+        if(Auth::attempt($param)){
+            session()->flash('success','登录成功');
+            return redirect()->route('users.show',[Auth::user()]);
+            echo 'ok';
+        }else{
+            session()->flash('danger','对不起,登录失败');
+            return redirect()->back()->withInput();
+        }
+    }
+}
